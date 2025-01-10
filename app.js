88fc2ae2e7ca9c54c2940ad0084c8b22175e5f76
b-hojayev <bgnchojayew@gmail.com>
2025-01-10 19:21:55 +0500

    initial setup

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//MIDDLEWARES
+app.use(express.json());
+
+//ROUTES
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("CONNECTED TO DB...");
+
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
